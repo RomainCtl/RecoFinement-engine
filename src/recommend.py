e93088fcb0f16ef1e7439647d0169886c3cc1ac4
RomainCtl <romain.chantrel35@gmail.com>
2020-11-12 23:25:28 +0100

    Recommend thread inherit from Engine

--- src/recommend.py
+++ src/recommend.py
@@ -1,49 +1,41 @@
 from src.engines import Popularity, ContentSimilarities, FromUserProfile
+from src.engines.engine import Engine
 
 from threading import Thread
 from datetime import datetime
 from flask import current_app
 
 
-class Recommend(Thread):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.app = current_app._get_current_object()
-        self.logger = current_app.logger
-
-    def run(self):
-        st_time = datetime.utcnow()
-
-        Popularity().start().join()
-        ContentSimilarities().start().join()
+class Recommend(Engine):
+    def train(self):
+        p = Popularity()
+        p.start()
+        p.join()
+        c = ContentSimilarities()
+        c.start()
+        c.join()
 
         # TODO Recommend content from similarities (from user top rating content)
         # TODO Recommend content from group profile
-        FromUserProfile().start().join()
+        fup = FromUserProfile()
+        fup.start()
+        fup.join()
         # TODO Collaboratif filtering
 
-        self.logger.info("%s engine performed in %s" %
-                         (self.__class__.__name__, datetime.utcnow()-st_time))
 
-
-class RecommendUser(Thread):
+class RecommendUser(Engine):
     def __init__(self, *args, user_uuid, **kwargs):
         super().__init__(*args, **kwargs)
-        self.app = current_app._get_current_object()
-        self.logger = current_app.logger
 
         self.user_uuid = user_uuid
 
         # raise an ValueError if invalid uuid format
         uuid.UUID(user_uuid)
 
-    def run(self):
-        st_time = datetime.utcnow()
-
+    def train(self):
         # TODO Recommend content from similarities (from user top rating content)
         # TODO Recommend content from group profile
-        FromUserProfile(user_uuid=self.user_uuid).start().join()
+        fup = FromUserProfile(user_uuid=self.user_uuid)
+        fup.start()
+        fup.join()
         # TODO Collaboratif filtering
-
-        self.logger.info("%s engine performed in %s" %
-                         (self.__class__.__name__, datetime.utcnow()-st_time))
